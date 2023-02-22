a1f0e122b916d4355084b617791fb6f98fad63fc
Andrea Torelli <gioran326@protonmail.com>
2023-02-22 15:07:31 +0100

    Help!

--- js/main.js
+++ js/main.js
@@ -9,12 +9,33 @@ va applicato uno sconto del 40% per gli over 65.
 L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 */
 
-const km = prompt("Quanti Km devi percorrere");
+const km = parseInt(prompt("Quanti Km devi percorrere"));
+const eta = parseInt(prompt("Quanti anni hai?"));
 
-const calcoloKm = (km * 0.21);
-const prezzoPieno = calcoloKm.toFixed(2);
-console.log("Il prezzo pieno è: " + prezzoPieno + "€");
+const prezzoPieno = (km * 0.21).toFixed(2);
+console.log("Il prezzo del biglietto è: " + prezzoPieno + "€");
 
+
+const sconto20 = (prezzoPieno / 100 * 20).toFixed(2); //per sapere di quanto verrà scontato il prezzo
+const sconto40 = (prezzoPieno / 100 * 40).toFixed(2); //per sapere di quanto verrà scontato il prezzo
+
+let prezzoFinale;
+
+if (eta <= 17) {
+   console.log("Ti facciamo uno sconto di: " + sconto20 + "€");
+   prezzoFinale = sconto20 + prezzoPieno;
+} else if (eta >= 65) {
+   console.log("Ti facciamo uno sconto di: " + sconto40 + "€");
+   prezzoFinale = sconto40 + prezzoPieno;
+} else {
+   console.log("Non sono previsti sconti!");
+   prezzoFinale = prezzoPieno;
+}
+
+console.log(prezzoFinale)
+
+
+// console.log("Il prezzo finale è: " + prezzoFinale + "€");
 
 
 
